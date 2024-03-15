bd3f84a55f3e784b4fdaa9dad221974f41224bbe
Jonas-Kunz <jonas_K_7@outlook.com>
2024-03-15 17:11:31 -0300

    Make cat walk

--- src/App.js
+++ src/App.js
@@ -1,107 +1,212 @@
-import './App.css';
-import {useEffect, useState} from 'react'
-import workBench from './components/WorkBench.js';
-import Phaser from 'phaser'
-import {DndContext, useDraggable, useDroppable} from '@dnd-kit/core';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import workBench from "./components/WorkBench.js";
+import Phaser from "phaser";
+import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
 
 function App() {
   let game;
-  let playerSpeed = 2;
-  
-  const [state, setState] = useState({counter: 0, loaded: false});
-  
+  let playerSpeed = 20;
 
+  const [state, setState] = useState({ counter: 0, loaded: false });
 
   function preload() {
-    this.load.setBaseURL('http://localhost:3000');
-    this.load.image('sky', 'assets/skies/sky.png');
-    this.load.spritesheet('player', 'assets/sprites/YeOldyNecroGuy.png', { frameWidth: 38, frameHeight: 38 });
-    this.load.image('ground', 'assets/ground/37623_grass_ground.png');
-    workBench.createFunction("jump", (jumpVelocity) => this.player.setVelocityY(-jumpVelocity));
+    this.load.setBaseURL("http://localhost:3000");
+    this.load.image("sky", "assets/skies/sky.png");
+    this.load.atlas(
+      "NinjaCat",
+      "assets/sprites/JsonArrayCat.png",
+      "assets/sprites/JsonArrayCat.json",
+      { frameWidth: 20, frameHeight: 48 }
+    );
+    this.load.image("ground", "assets/ground/spritesheet_ground.png");
+    this.load.tilemapTiledJSON(
+      "tilemap",
+      "assets/maps/level1/FirstAttempt.json"
+    );
+    workBench.createFunction("jump", (jumpVelocity) =>
+      this.player.setVelocityY(-jumpVelocity)
+    );
   }
 
   function create() {
-    this.add.image(400, 300, 'sky');
-    this.player = this.physics.add.sprite(120,350,'player').setScale(4);
-    this.player.setCollideWorldBounds(true)
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.physicsBodyType = Phaser.Physics.ARCADE;
+    this.add.image(400, 300, "sky").setScale(100);
 
-    let ground = this.platforms.create(100, 600, 'ground').setScale(1);
-    ground.body.setSize(2000,80)
-    
-    this.physics.add.collider(this.player, this.platforms);
+    const map = this.make.tilemap({ key: "tilemap" });
+    const tileSet1 = map.addTilesetImage("spritesheet_ground", "ground");
+    const ground = map.createLayer("ground", tileSet1, 0, 0);
 
-    this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    ground.setCollisionByExclusion([-1]);
+
+    this.physics.world.bounds.width = ground.width;
+    this.physics.world.bounds.height = ground.height;
+
+    this.player = this.physics.add.sprite(120, 980, "NinjaCat");
+    this.player.setBounce(0.2);
+    this.player.body.setSize(80, 190);
+    this.player.setOffset(40, 20);
+
+    this.player.setCollideWorldBounds(true);
+
+    this.cameras.main.setBounds(0, 0, ground.width, ground.height);
+    this.cameras.main.startFollow(this.player);
+
+    this.physics.add.collider(this.player, ground);
+
     this.one = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
     this.two = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
-    this.three = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
+    this.three = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.THREE
+    );
     this.four = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
     this.five = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
     this.six = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SIX);
-    this.seven = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEVEN);
-    this.eight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.EIGHT);
+    this.seven = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SEVEN
+    );
+    this.eight = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.EIGHT
+    );
 
-    
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+      key: "player-walk",
+      framreate: 10,
+      frames: this.anims.generateFrameNames("NinjaCat", {
+        start: 1,
+        end: 8,
+        prefix: "NinjaCat_walk_0",
+        suffix: ".png",
+      }),
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "player-idle",
+      framreate: 10,
+      frames: this.anims.generateFrameNames("NinjaCat", {
+        start: 1,
+        end: 3,
+        prefix: "NinjaCat_idle_0",
+        suffix: ".png",
+      }),
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "player-jump",
+      framrate: 2,
+      frames: this.anims.generateFrameNames("NinjaCat", {
+        start: 1,
+        end: 6,
+        prefix: "NinjaCat_jump_0",
+        suffix: ".png",
+      }),
+    });
     // setState((previous) => {
     //   return {...previous, counter: previous.counter + 1 }
     // }); //note, just a quick way to force a reload for the workbench
   }
 
   function update(now, delta) {
-    if (this.left.isDown) {
-      this.player.x -= playerSpeed
-    } else if (this.right.isDown) {
-      this.player.x += playerSpeed
+    let player = this.player;
+    let horizontalVelocity = 0;
+
+    // Check if the player is in the air
+    let airControlFactor = player.body.onFloor() ? 1 : 0.2;
+
+    if (this.cursors.left.isDown) {
+      if (player.body.onFloor()) {
+        player.setOffset(133, 20)
+        player.body.setVelocityX(-500);
+        player.anims.play("player-walk", true);
+        player.setFlipX(true);
+      } else {
+        player.setFlipX(true);
+        this.player.setOffset(133, 20)
+        // Apply a smaller change in velocity for air control
+        player.body.velocity.x -= 10;
+      }
+      
+    }
+    // Moving right
+    else if (this.cursors.right.isDown) {
+      if (player.body.onFloor()) {
+        player.body.setVelocityX(500);
+        player.setOffset(40, 20);
+        player.anims.play("player-walk", true);
+        player.setFlipX(false);
+      } else {
+        player.setFlipX(false);
+        this.player.setOffset(40, 20);
+        // Apply a smaller change in velocity for air control
+        player.body.velocity.x += 10;
+      }
+      
+      
+    }
+    // Idle state or air control
+    else {
+      if (player.body.onFloor()) {
+        player.anims.play("player-idle", true);
+        player.anims.msPerFrame = 500;
+        player.body.setVelocityX(0); // Stop horizontal movement if not pressing left or right
+      }
     }
 
-    if ((this.spacebar.isDown) && (this.player.body.touching.down)) {
-      // this.player.setVelocityY(-200);
-      workBench.execute1();
+    // Jumping
+    if (this.cursors.up.isDown /*&& player.body.onFloor()*/) {
+      player.body.setVelocityY(-500); // Apply vertical velocity to jump
+      player.anims.play("player-jump", true);
     }
+
+    // Cap the player's horizontal velocity to avoid unrealistic speeds
+    player.body.velocity.x = Phaser.Math.Clamp(
+      player.body.velocity.x,
+      -500,
+      500
+    );
 
     if (this.one.isDown) {
-      console.log('one')
+      console.log("one");
     }
-
   }
 
   useEffect(() => {
-    if (!state.loaded) {  //if statement needed so that strictmode doesn't make 2 canvases
-      console.log('render game')
+    if (!state.loaded) {
+      //if statement needed so that strictmode doesn't make 2 canvases
+      console.log("render game");
       setState((previous) => {
-        return {...previous, loaded: true }
-      })
+        return { ...previous, loaded: true };
+      });
       game = new Phaser.Game(gameConfig);
-    } 
+    }
   }, []);
 
   const gameConfig = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    parent: 'phaser',
+    parent: "phaser",
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 800 }
-        }
+      default: "arcade",
+      arcade: {
+        gravity: { y: 800 },
+        debug: true,
+      },
     },
     scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-  }
+      preload: preload,
+      create: create,
+      update: update,
+    },
+  };
 
-  
   return (
     <div className="mainWindow">
       <div className="App">{workBench.getReactBench()}</div>
-      
+
       <div id="phaser"></div>
     </div>
   );
